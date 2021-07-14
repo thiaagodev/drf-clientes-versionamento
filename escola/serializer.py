27c46a5f0ae9a5ea2fb7a912a90c6e5fea71c058
Thiago Ferreira <thiaago.dev@gmail.com>
2021-07-14 17:46:32 -0300

    Criando versão 2 do Viewset de alunos com o query paramter versioning

--- escola/serializer.py
+++ escola/serializer.py
@@ -30,3 +30,9 @@ class ListaAlunosMatriculadosSerializer(serializers.ModelSerializer):
     class Meta:
         model = Matricula
         fields = ['aluno_nome']
+
+
+class AlunoSerializerV2(serializers.ModelSerializer):
+    class Meta:
+        model = Aluno
+        fields = ['id', 'nome',  'celular', 'rg', 'cpf', 'data_nascimento']
