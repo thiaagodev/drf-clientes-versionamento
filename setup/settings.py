56fb5d2966683e5dcb86d3dbbb0ecbd430ba38b2
Thiago Ferreira <thiaago.dev@gmail.com>
2021-07-15 17:18:09 -0300

    CORS

--- setup/settings.py
+++ setup/settings.py
@@ -38,12 +38,14 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     'escola',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -137,3 +139,7 @@ REST_FRAMEWORK = {
     #     'anon': '100/day',
     # }
 }
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+]
