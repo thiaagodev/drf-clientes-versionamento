c4ec2f712b15b7f3a3390919e1528f74fad51322
Thiago Ferreira <thiaago.dev@gmail.com>
2021-07-15 18:26:54 -0300

    Configurando REDIS no Django

--- setup/settings.py
+++ setup/settings.py
@@ -143,6 +143,19 @@ REST_FRAMEWORK = {
     # }
 }
 
+# CACHES = {
+#     "default": {
+#         "BACKEND": "django_redis.cache.RedisCache",
+#         "LOCATION": "redis://127.0.0.1:6379/1",
+#         "OPTIONS": {
+#             "CLIENT_CLASS": "django_redis.client.DefaultClient",
+#         }
+#     }
+# }
+
+# SESSION_ENGINE = "django.contrib.sessions.backends.cache"
+# SESSION_CACHE_ALIAS = "default"
+
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
 ]
