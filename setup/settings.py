57d41a1fc7094168c482af4c32df94ad61072b6e
Thiago Ferreira <thiaago.dev@gmail.com>
2021-07-16 17:34:49 -0300

    Alterando endereço de admin para garantir maior segurança

--- setup/settings.py
+++ setup/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'we2q@@2p30+=^#(xbgl7e@3+r2dt+zmf#us2m$ix=q&n+=y-z$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost']
 
 
 # Application definition
